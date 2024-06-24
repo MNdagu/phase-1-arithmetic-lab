1d96436c41ba9b505bf295a789d04576b47d2eaa
MNdagu <ndagumark@gmail.com>
2024-06-24 18:59:35 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+//is an equation whose multiplied results will equal 62
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply);
+
+//generates a random integer greater than 0
+const random = Math.round(Math.random() * 10 + 1);
+Number.isInteger(random);
+console.log(random);
+
+//is an equation that calculates a remainder that is equal to 4
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+//will return 20 as the highest number in the set
+const max = Math.max(1, 5, 10, 20);
+console.log(max);
